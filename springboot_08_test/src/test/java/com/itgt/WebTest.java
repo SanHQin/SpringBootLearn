8513b27425083584e083a7125d1c3cce477cd0ba
SanHQin <3343521716@qq.com>
2023-08-13 22:57:46 +0800

    unknown

--- springboot_08_test/src/test/java/com/itgt/WebTest.java
+++ springboot_08_test/src/test/java/com/itgt/WebTest.java
@@ -6,19 +6,75 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.ContentResultMatchers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class WebTest {
 
     @Test
-    //注入虚拟MVC调用对象
-    void testWeb(@Autowired MockMvc mvc) throws Exception {
+        //注入虚拟MVC调用对象
+    void testStatus(@Autowired MockMvc mvc) throws Exception {
         //创建虚拟请求，当前访问/books
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
         //执行对应的请求
-        mvc.perform(builder);
+        ResultActions action = mvc.perform(builder);
+
+//        mvc.perform(builder);
+
+        //设置预期值 与真实值进行比较，成功测试通过，失败测试失败
+        //定义本次调用的预期值
+        StatusResultMatchers status = MockMvcResultMatchers.status();
+        //预计本次调用成功的状态：200
+        ResultMatcher ok = status.isOk();
+        //添加预计值到本次调用过程中进行匹配
+        action.andExpect(ok);
+
+    }
+
+    @Test
+        //注入虚拟MVC调用对象
+    void testBody(@Autowired MockMvc mvc) throws Exception {
+        //创建虚拟请求，当前访问/books
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
+        //执行对应的请求
+        ResultActions action = mvc.perform(builder);
+
+//        mvc.perform(builder);
+
+        //设置预期值 与真实值进行比较，成功测试通过，失败测试失败
+        //定义本次调用的预期值
+        ContentResultMatchers content = MockMvcResultMatchers.content();
+        //预计本次调用成功的值
+        ResultMatcher result = content.string("getById is running.....");
+        //添加预计值到本次调用过程中进行匹配
+        action.andExpect(result);
+
+    }
+
+    @Test
+        //注入虚拟MVC调用对象
+    void testJson(@Autowired MockMvc mvc) throws Exception {
+        //创建虚拟请求，当前访问/books
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
+        //执行对应的请求
+        ResultActions action = mvc.perform(builder);
+
+//        mvc.perform(builder);
+
+        //设置预期值 与真实值进行比较，成功测试通过，失败测试失败
+        //定义本次调用的预期值
+        ContentResultMatchers content = MockMvcResultMatchers.content();
+        //预计本次调用成功的值
+        ResultMatcher result = content.json("");
+        //添加预计值到本次调用过程中进行匹配
+        action.andExpect(result);
+
     }
 }
---
+++ springboot_09_h2sql/src/main/java/com/itgt/Springboot08H2sqlApplication.java
@@ -0,0 +1,13 @@
+package com.itgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot08H2sqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot08H2sqlApplication.class, args);
+    }
+
+}
